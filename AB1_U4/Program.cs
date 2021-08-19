45aea86989df0f991ec82335c23bb6f18619be98
The man <ThisIsntTheWay@users.noreply.github.com>
2021-08-19 20:21:59 +0200

    Update Program.cs

--- AB1_U4/Program.cs
+++ AB1_U4/Program.cs
@@ -16,47 +16,58 @@ namespace school_programming
             Console.WriteLine("Awaiting input (1-45)");
 
             // Prompt for input
-            for (int i = 0; i <= 5; i++) {
-                bool canContinue = false;
-                while (!canContinue) {
-                    Console.Write("Enter number #" + (i+1) + ": ");
-                    var e = Console.ReadLine();
-                    int input = int.Parse(e);
+            bool gameActive = true;
+            while (gameActive) {
+    for (int i = 0; i <= 5; i++) {
+                    bool canContinue = false;
+                    while (!canContinue) {
+                        Console.Write("Enter number #" + (i+1) + ": ");
+                        var e = Console.ReadLine();
+                        int input = int.Parse(e);
 
-                    if (input > 45) {
-                        Console.WriteLine("Input range exceeded.");
-                    } else {
-                        userInput.Add(input);
-                        canContinue = true;
+                        if (input > 45) {
+                            Console.WriteLine("Input range exceeded.");
+                        } else {
+                            userInput.Add(input);
+                            canContinue = true;
+                        }
                     }
                 }
 
-            }
+                // Present some shit
+                var rand = new Random();
+                for (int i = 0; i <= 6; i++) {
+                    lottoList.Add(rand.Next(45));
+                }
 
-            // Present some shit
-            var rand = new Random();
-            for (int i = 0; i <= 6; i++) {
-                lottoList.Add(rand.Next(45));
-            }
+                Console.Write("Random numbers: ");
+                returnList(lottoList);
+                Console.WriteLine();
 
-            Console.Write("Random numbers: ");
-            returnList(lottoList);
-            Console.WriteLine();
+                foreach (int lottoNumber in lottoList) {
+                    if (userInput.Contains(lottoNumber)) {
+                        matchList.Add(lottoNumber);
+                    }
+                }
 
-            foreach (int lottoNumber in lottoList) {
-                if (userInput.Contains(lottoNumber)) {
-                    matchList.Add(lottoNumber);
+                if (matchList.Count <= 0) {
+                    Console.Write("You matched these numbers: ");
+                    returnList(matchList);
+                } else {
+                    Console.WriteLine("No number");
+                }
+
+                Console.WriteLine();
+
+                Console.Write("Retry? (y/n): ");
+                if (Console.ReadLine() != "y") {
+                    gameActive = false;
+                } else {
+                    userInput.Clear();
+                    lottoList.Clear();
+                    matchList.Clear();
                 }
             }
-
-            if (matchList.Count <= 0) {
-                Console.Write("You matched these numbers: ");
-                returnList(matchList);
-            } else {
-                Console.WriteLine("No number");
-            }
-
-            Console.WriteLine();
         }
 
         public static void returnList( List<int> myCol )  {
