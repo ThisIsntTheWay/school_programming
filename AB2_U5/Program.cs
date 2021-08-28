e487551c970bb02c52ca5d61952877ce977b3816
The man <ThisIsntTheWay@users.noreply.github.com>
2021-08-28 12:34:22 +0200

    AB2, U5

---
+++ AB2_U5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AB2_U5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Write("Please provide a number: ");
+                int i = Convert.ToInt32(Console.ReadLine());
+
+                String output = String.Format("{0:0.00}", Math.Sqrt(i));
+                Console.WriteLine("The root of this number is: " + output);
+            }
+        }
+    }
+}
