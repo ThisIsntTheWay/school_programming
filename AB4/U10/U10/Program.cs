8e91a90984ad284b3c21873279e116d182ef24d0
The man <ThisIsntTheWay@users.noreply.github.com>
2021-09-09 21:44:39 +0200

    Add JSON serialization, implement "outputMode"

--- AB4/U10/U10/Program.cs
+++ AB4/U10/U10/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 using Tsbe.Raumfahrt;
 
 namespace U10
@@ -12,7 +13,10 @@ namespace U10
         private static List<Raumschiff> Shuttles = new List<Raumschiff>();
         private static List<Land> Countries = new List<Land>();
 
-        private static bool proMode = true;
+        private static string outputRaw = @"..\..\..\output.dat";
+        private static string outputJSON = @"..\..\..\output.json";
+
+        private static int outputMode = 2;
 
         static void Main(string[] args)
         {
@@ -49,31 +53,47 @@ namespace U10
             });
 
             // Prompt to console
-            if (!proMode) {
-                Console.WriteLine("Pro mode is deactivated.`n");
-                foreach (var shuttle in Shuttles) {
-                    // This could be heaviliy shortened if the OS culture accepts 'yyyyMMdd' as a standard time format - which mine does not >:(
-                    DateTime einsatzVon; DateTime.TryParseExact((shuttle.EinsatzVon).ToString(), "yyyyMMdd", CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out einsatzVon);
-                    DateTime einsatzBis; DateTime.TryParseExact((shuttle.EinsatzBis).ToString(), "yyyyMMdd", CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out einsatzBis);
-                    var zeitspanne = (einsatzBis - einsatzVon).TotalDays;
+            Console.WriteLine("Output mode: {0}", outputMode);
 
-                    Console.WriteLine("Programm: {0}", shuttle.Programm);
-                    Console.WriteLine("Launch vehicle: {0}", shuttle.Traegerrakete);
-                    Console.WriteLine("Country: {0}", shuttle.Land.Name);
-                    Console.WriteLine("Mission star: {0}", einsatzVon);
-                    Console.WriteLine("Mission end:  {0}", einsatzBis);
-                    Console.WriteLine("Mission duration: {0} (days)", zeitspanne);
-                    Console.WriteLine(" ");
-                }
-            } 
-            
-            // Pro 10.1 (Raw datastream serialization)
-            else {
-                BinaryFormatter binFormat = new BinaryFormatter();
-                var memStream = new MemoryStream();
+            switch (outputMode) {
+                case 0:     // Normal execution
+                    foreach (var shuttle in Shuttles) {
+                        // This could be heaviliy shortened if the OS culture accepts 'yyyyMMdd' as a standard time format - which mine does not >:(
+                        DateTime einsatzVon; DateTime.TryParseExact((shuttle.EinsatzVon).ToString(), "yyyyMMdd", CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out einsatzVon);
+                        DateTime einsatzBis; DateTime.TryParseExact((shuttle.EinsatzBis).ToString(), "yyyyMMdd", CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out einsatzBis);
+                        var zeitspanne = (einsatzBis - einsatzVon).TotalDays;
 
-                binFormat.Serialize(memStream, Shuttles);
-                File.WriteAllBytes(@".\output.dat", memStream.ToArray());
+                        Console.WriteLine("Programm: {0}", shuttle.Programm);
+                        Console.WriteLine("Launch vehicle: {0}", shuttle.Traegerrakete);
+                        Console.WriteLine("Country: {0}", shuttle.Land.Name);
+                        Console.WriteLine("Mission star: {0}", einsatzVon);
+                        Console.WriteLine("Mission end:  {0}", einsatzBis);
+                        Console.WriteLine("Mission duration: {0} (days)", zeitspanne);
+                        Console.WriteLine(" ");
+                    }
+
+                    break;
+                
+                case 1:     // Pro 10.1 (Raw datastream serialization)
+                    BinaryFormatter binFormat = new BinaryFormatter();
+                    var memStream = new MemoryStream();
+
+                    //binFormat.Serialize(memStream, Shuttles);
+                        // Crashes due to non-serializable class (Tsbe.Raumschiff)
+                    File.WriteAllBytes(outputRaw, memStream.ToArray());
+                    Console.WriteLine("Done serializing raw.");
+
+                    break;
+
+                case 2:     // Pro 10.2 (JSON serialization)
+                    using (StreamWriter file = File.CreateText(outputJSON))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        serializer.Serialize(file, Shuttles);
+                    }
+
+                    Console.WriteLine("Done serializing JSON.");
+                    break;
             }
         }
     }
