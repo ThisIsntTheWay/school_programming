b7ce878a5f7b362c856ae1b9594c343de74e9b8b
The Man <ThisIsntTheWay@users.noreply.github.com>
2021-11-22 22:13:02 +0100

    Update Program.cs

--- AB5/Classes/Program.cs
+++ AB5/Classes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Classes
 {
@@ -6,30 +7,42 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Supercomputer fätzecompi = new Supercomputer();
+            List<Country> CountryList = new List<Country>();
+            List<Supercomputer> ComputerList = new List<Supercomputer>();
+
+            CountryList.Add(new Country { Name = "United States", ISO3 = "USA", Population = 328200000, Capital = "Washington, D.C." });
+            CountryList.Add(new Country { Name = "Japan", ISO3 = "JP", Population = 125800000, Capital = "Tokyo" });
+
+            ComputerList.Add(new Supercomputer {
+                Name = "IBM Summit",
+                CpuCount = 9216,
+                CoresPerCpu = 22,
+                Petaflops = 200,
+                Country = CountryList.Find(c => c.ISO3 == "USA"),
+                Location = "Oak Ridge, U.S."
+            });
+            ComputerList.Add(new Supercomputer {
+                Name = "Fugaku",
+                CpuCount = 158976,
+                CoresPerCpu = 52,
+                Petaflops = 442,
+                Country = CountryList.Find(c => c.ISO3 == "JP"),
+                Location = "Riken Center, Kobe"
+            });
 
             // Uncomment to throw exception
             //supercomputer.Name = "awdawuidzawiudawiudzawiuzdawiuzdiuawzda";
-            fätzecompi.Name = "IBM Summit";
-            fätzecompi.CpuCount = 9216;
-            fätzecompi.CoresPerCpu = 22;
-            fätzecompi.Petaflops = 200;
-            fätzecompi.Country = new Country() {
-                Name = "United States",
-                ISO3 = "USA",
-                Population = 328200000,
-                Capital = "Washington, D.C."
-            };
-            fätzecompi.Location = "Oak Ridge, U.S.";
-
-            Console.WriteLine("Fätzecompi \"{0}\":", fätzecompi.Name);
-            Console.WriteLine("> CPUs: {0}", fätzecompi.CpuCount);
-            Console.WriteLine("> Cores per CPU: {0}", fätzecompi.CoresPerCpu);
-            Console.WriteLine("  > Cores total: {0}", fätzecompi.Cores);
-            Console.WriteLine("> PetaFlops: {0}", fätzecompi.Petaflops);
-            Console.WriteLine("  > TeraFLOPS per core: {0}", fätzecompi.FlopsPerCore);
-            Console.WriteLine("> Country: {0} / {1}", fätzecompi.Country.Name, fätzecompi.Country.ISO3);
-            Console.WriteLine("> Location: {0}", fätzecompi.Location);
+            foreach (var Computer in ComputerList)  {
+                Console.WriteLine("Computer \"{0}\":", Computer.Name);
+                Console.WriteLine("> CPUs: {0}", Computer.CpuCount);
+                Console.WriteLine("> Cores per CPU: {0}", Computer.CoresPerCpu);
+                Console.WriteLine("  > Cores total: {0}", Computer.Cores);
+                Console.WriteLine("> PetaFlops: {0}", Computer.Petaflops);
+                Console.WriteLine("  > TeraFLOPS per core: {0}", Computer.FlopsPerCore);
+                Console.WriteLine("> Country: {0} / {1}", Computer.Country.Name, Computer.Country.ISO3);
+                Console.WriteLine("  > Location: {0}", Computer.Location);
+                Console.WriteLine(" ");
+            }
         }
     }
 }
